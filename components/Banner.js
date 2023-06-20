50fd0e68549143097ee85d672e915f27c14c08ce
ritikkatiyar <ritik.katiyar79.hk@gmail.com>
2023-06-20 17:48:19 +0530

    'ui'

--- components/Banner.js
+++ components/Banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
         <button className="text-purple-500 bg-white shadow-md rounded-full font-bold my-3 px-10 py-4 hover:shadow-xl active:scale-90 transition duration-150">
-          I'm flexible
+          I&apos;m flexible
         </button>
       </div>
     </div>
